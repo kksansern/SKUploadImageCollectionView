1bb8dc21c01c2f3877074b0aa403dbcf52b2708f
sansern kukkong <sansern.kukkong@gmail.com>
2019-06-13 10:58:16 +0700

    create template version

---
+++ SKUploadImageCollectionView/SKUploadImageCollectionView/SKUploadImageCollectionViewCell/SKUploadImageCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SKUploadImageCollectionViewCell.swift
+//  SKUploadImageCollectionViewExample
+//
+//  Created by admin on 6/12/19.
+//  Copyright © 2019 sk. All rights reserved.
+//
+
+import Foundation
